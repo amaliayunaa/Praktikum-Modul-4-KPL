d129281afe6d845d511503dc8d45391097a6e106
amaliayunaa <malyayn20@gmail.com>
2022-03-19 10:07:44 +0700

    menambahkan class Penjumlahan dengan methodJumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302204100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nim 1302204100");
+            Console.Write("Penjumlahan Long : ");
+            Console.WriteLine(Penjumlahan.jlh<long>(13, 02, 20));
+        }
+    }
+
+    class Penjumlahan
+    {
+        public static T jlh<T>(T satu, T dua, T tiga)
+        {
+            return (dynamic)satu + (dynamic)dua + (dynamic)tiga;
+        }
+    }
+}
+
 
